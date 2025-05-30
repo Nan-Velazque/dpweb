e75ae72159eee147ba44a6c51766e898c6fa6825
Nan-Velazque <huamannancy096@gmail.com>
2025-05-30 12:18:52 -0500

    nancy

---
+++ config/config copy.php
@@ -0,0 +1,11 @@
+<?php
+const BD_HOST = "localhot";
+const BD_NAME = "";
+const BD_USER = "";
+const BD_PASSAWORD = "";
+const BD_CHARSET = "utff8";
+
+//Ingresar link de proyecto
+const BASE_URL = "";
+
+?>
