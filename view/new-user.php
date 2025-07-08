fb74c7b101e121b2c609c968c64a49641e824dfb
Nan-Velazque <huamannancy096@gmail.com>
2025-07-08 12:51:55 -0500

    8/07/25

--- view/new-user.php
+++ view/new-user.php
@@ -1,84 +1,18 @@
-<!DOCTYPE html>
-<html lang="es">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Esther</title>
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>view/bootstrap/css/bootstrap.min.css">
-    <script>
-        const base_url = '<?php echo BASE_URL; ?>';
-    </script>
-
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-lg" style="background-color:rgb(166, 219, 243);">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Logo</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Users</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Products</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="<?php echo BASE_URL; ?>categories">Categories</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Clients</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Shops</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Sales</a>
-                    </li>
-
-                </ul>
-                <form class="d-flex" role="search">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-
-
-                    </ul>
-                </form>
-            </div>
-        </div>
-    </nav>
+    <!--Inicio de cuerpo de pagina-->
     <div class="container-fluid">
         <div class="card">
-            <h5 class="card-header">titulo</h5>
+            <h5 class="card-header">Registro de Usuario</h5>
             <form id="frm_user" action="" method="">
                 <div class="card-body">
                     <div class="mb-3 row">
-                        <label for="nro_identidad" class="col-sm-4 col-form-label">Nro de Documento :</label>
+                        <label for="nro_identidad" class="col-sm-4 col-form-label"> Nro de identidad :</label>
                         <div class="col-sm-8">
                             <input type="number" class="form-control" id="nro_identidad" name="nro_identidad" required>
                         </div>
                     </div>
                     <div class="mb-3 row">
-                        <label for="razon_social" class="col-sm-4 col-form-label">Razon social :</label>
+                        <label for="razon_social" class="col-sm-4 col-form-label">Razon social:</label>
                         <div class="col-sm-8">
                             <input type="text" class="form-control" id="razon_social" name="razon_social" required>
                         </div>
@@ -129,11 +63,10 @@
                         <label for="rol" class="col-sm-4 col-form-label">Rol :</label>
                         <div class="col-sm-8">
                             <select class="form-control" name="rol" id="rol" required>
-                                <option disabled selected>seleccionar rol</option>
+                                <option disabled selected>seleccione</option>
                                 <option value="1">administrador</option>
                                 <option value="2">usuario</option>
-                                <option value="3">contador</option>
-                                <option value="4">almacenero</option>
+                                <option value="3">almacen</option>
                             </select>
                         </div>
                     </div>
@@ -146,11 +79,5 @@
     </div>
     </div>
 
-
-</body>
-<script src="<?php echo BASE_URL; ?>view/function/user.js"></script>
-<script src="<?php echo BASE_URL; ?>view/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-</html>
+    <!--Fin de cuerpo de pagina-->
+    <script src="<?php echo BASE_URL; ?>view/function/user.js"></script>
