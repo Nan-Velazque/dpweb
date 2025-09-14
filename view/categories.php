0976ca4c2d8a6e9bd5e493741a997e785c358ca0
Nan_Velazque <huamannancy096@gmail.com>
2025-09-14 05:09:11 -1200

    categories.php

---
+++ view/categories.php
@@ -0,0 +1,18 @@
+<div id="form-control">
+<div class="col-12">
+    <table border="1" style="width:100%">
+        <thead>
+            <tr>
+                <th>Nro.</th>
+                <th>Nombre</th>
+                <th>Detalles</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody id="tbl_categoria">
+        </tbody>
+    </table>
+</div>
+</div>
+<script src="<?= BASE_URL ?>view/function/categoria.js"></script>
+<!--<script>view_categories();</script>-->
