52e5304c8467239e3501c168c5638346114e47ab
Nan_Velazque <huamannancy096@gmail.com>
2025-09-14 05:34:42 -1200

    footer.php

--- view/include/footer.php
+++ view/include/footer.php
@@ -2,5 +2,8 @@
 <script src="<?php echo BASE_URL; ?>view/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
 
 </html>
