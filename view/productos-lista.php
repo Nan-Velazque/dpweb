6e99dc1f516e181195f688d1b3008e79e0d731be
Nan-Velazque <huamannancy096@gmail.com>
2025-12-04 11:51:10 -0500

    eede

--- view/productos-lista.php
+++ view/productos-lista.php
@@ -11,28 +11,29 @@
       <div class="table-responsive">
 
         <table class="table table-hover align-middle text-center"
-          style="border: none !important;">
+          >
 
-          <thead style="background:#e6dcff; color:#5e4a80;">
+          <thead >
             <tr>
-              <th style="border:none;">Nro</th>
-              <th style="border:none;">Código</th>
-              <th style="border:none;">Nombre</th>
-              <th style="border:none;">Precio</th>
-              <th style="border:none;">Stock</th>
-              <th style="border:none;">Categoría</th>
-              <th style="border:none;">Proveedor</th>
-              <th style="border:none;">Vencimiento</th>
-              <th style="border:none;">Código Barra</th>
-              <th style="border:none;">Acciones</th>
+              <th>Nro</th>
+              <th>Código</th>
+              <th>Nombre</th>
+              <th>Precio</th>
+              <th>Stock</th>
+              <th>Categoría</th>
+              <th>Proveedor</th>
+              <th>Vencimiento</th>
+              <th>Código Barra</th>
+              <th>Acciones</th>
             </tr>
           </thead>
 
-          <tbody id="content_productos">
+          <tbody id="content_productos" >
             <!-- Filas dinámicas generadas con JavaScript -->
           </tbody>
 
         </table>
+
 
       </div>
     </div>
